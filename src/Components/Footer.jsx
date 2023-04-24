a7f254711b5ee88ebc71c87730cb90314ba8e725
nishantp15 <prajapati.nishant@outlook.com>
2023-04-24 23:46:51 +0530

    classes modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,11 +6,8 @@ function Footer() {
     <div><h4>Thank you for visiting !</h4></div>
     <div><RiCopyrightLine/> &nbsp; <p> 2023 All rights reserved.</p></div>
     <div>Designed by Nishant Prajapati</div>
-    <div>Last updated: 11-Feb, 2023</div>
-    <p id="github-streak-stats" ><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=nishantp15&show_icons=true&locale=en&layout=compact" alt="nishantp15" /></p>
-
-<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=nishantp15&show_icons=true&locale=en" alt="nishantp15" /></p>
-
+    <div>Last updated: 24-04-2023</div>
+    
   </div>;
 }
 
