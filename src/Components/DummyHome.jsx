470add731b97559a1357b243f547b32f89846951
nishantp15 <prajapati.nishant@outlook.com>
2023-04-24 23:35:31 +0530

    classes modified

--- src/Components/DummyHome.jsx
+++ src/Components/DummyHome.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import resume from '../Media/Nishant_Prajapati_Resume_Fullstack_Web_Dev.pdf'
 const DummyHome = () => {
+    function DownloadResume(){
+  
+        const anchor = document.createElement('a');
+        anchor.href = resume;
+        anchor.setAttribute('download','Resume_Nishant_Prajapati_FullStack_developer');
+        document.body.appendChild(anchor);
+        anchor.click();
+        anchor.remove();
+       
+      
+      }
   return (
-    <div id="home"></div>
+    <div id="home"><button onClick={DownloadResume} id="resume-button-2">Download Resume</button></div>
   )
 }
 
