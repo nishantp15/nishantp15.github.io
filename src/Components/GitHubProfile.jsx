dc4552a280330168821f6156f919e739445d5b2d
nishantp15 <prajapati.nishant@outlook.com>
2023-04-25 12:15:27 +0530

    GitHub Added

--- src/Components/GitHubProfile.jsx
+++ src/Components/GitHubProfile.jsx
@@ -4,7 +4,9 @@ const GitHubProfile = () => {
   return (
     <div className="GitHub">
       <h1>GitHub Details</h1>
+      <div className="react-activity-calendar"></div>
       <div>
+        
         <div ><img id="github-streak-stats" src="https://github-readme-streak-stats.herokuapp.com?user=nishantp15" alt="" /></div>
         <div ><img id="github-top-langs" src="https://github-readme-stats.vercel.app/api/top-langs?username=nishantp15&show_icons=true&locale=en&layout=compact" alt="" /></div>
         <div><img id="github-stats-card" src="https://github-readme-stats.vercel.app/api?username=nishantp15" alt="" /></div>
