644483ca0e9a2edbceceb693c642f3d80f67317d
nishantp15 <prajapati.nishant@outlook.com>
2023-04-28 19:36:33 +0530

    Download modified

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -20,7 +20,6 @@ function About() {
   //   anchor.click();
   //   anchor.remove();
   //   window.open(newPageUrl, '_blank')
-    
   // }
 
 
--- src/Components/ResumeLink.js
+++ src/Components/ResumeLink.js
@@ -1,9 +1,9 @@
+import resume from '../Media/Nishant_Prajapati_Resume_Fullstack_Web_Dev.pdf'
 function DownloadResume(e) {
     e.preventDefault();
     const downloadUrl = `https://drive.google.com/uc?export=download&id=1jTLnglKOPH4NuEa6TY7IrSiqkf8tpkxa`
     const newPageUrl = `https://drive.google.com/file/d/1jTLnglKOPH4NuEa6TY7IrSiqkf8tpkxa/view?usp=share_link`
    
-    e.preventDefault();
     const anchor = document.createElement("a");
     anchor.href = downloadUrl;
     anchor.setAttribute(
@@ -13,7 +13,7 @@ function DownloadResume(e) {
     document.body.appendChild(anchor);
     anchor.click();
     anchor.remove();
-    window.open(newPageUrl, '_blank')
+    window.open(newPageUrl)
     
   }
 
