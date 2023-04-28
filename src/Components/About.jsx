adc81fdcfac1f9b54c57dacb58b7952fe2736731
nishantp15 <prajapati.nishant@outlook.com>
2023-04-28 21:15:21 +0530

    Download modified

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,27 +1,10 @@
 import React from "react";
 import "./Styles/About.css";
 import profileImage from "../Media/download-removebg-preview (1).jpg";
-// import resume from '../Media/Nishant_Prajapati_Resume_Fullstack_Web_Dev.pdf'
 import DownloadResume from "./ResumeLink";
-// import { Button } from '@chakra-ui/react'
-function About() {
-  // function DownloadResume(e) {
-  //   e.preventDefault();
-  //   const downloadUrl = `https://drive.google.com/uc?export=download&id=1jTLnglKOPH4NuEa6TY7IrSiqkf8tpkxa`
-  //   const newPageUrl = `https://drive.google.com/file/d/1jTLnglKOPH4NuEa6TY7IrSiqkf8tpkxa/view?usp=share_link`
-  //   window.open(newPageUrl, '_blank')
-  //  window.location.href=downloadUrl
-  //   const anchor = document.createElement("a");
-  //   anchor.href = resume;
-  //   anchor.setAttribute(
-  //     "download",
-  //     "Resume_Nishant_Prajapati_FullStack_developer"
-  //   );
-  //   document.body.appendChild(anchor);
-  //   anchor.click();
-  //   anchor.remove();
-  // }
 
+function About() {
+  
   return (
     <div id="about" className="about section">
       <div id="welcome">
